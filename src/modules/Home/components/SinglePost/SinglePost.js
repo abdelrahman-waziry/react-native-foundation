00827c475ea8b24c8972c1cb478257f786f0adcb
abdelrahman-waziry <abdelrahman.waziry@gmail.com>
2020-05-24 14:42:46 +0200

    Add download option to singlepost

--- src/modules/Home/components/SinglePost/SinglePost.js
+++ src/modules/Home/components/SinglePost/SinglePost.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, ImageBackground, TouchableHighlight } from 'react-native'
+import { View, ImageBackground, TouchableHighlight, Image, Text } from 'react-native'
+import * as FileSystem from 'expo-file-system';
 import {AntDesign} from '@expo/vector-icons'
 import { styles } from './style'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler'
 
 class SinglePost extends React.Component {
     constructor(props){
@@ -15,10 +16,11 @@ class SinglePost extends React.Component {
     render(){
         const {item} = this.props.route.params
         return(
-            <View style={styles.singlePostWrapper}>
+            <ScrollView style={styles.singlePostWrapper}>
                 <ImageBackground
                     resizeMode='cover'
                     style={styles.singlePostImage}
+                    imageStyle={{borderBottomRightRadius: 35, borderBottomLeftRadius: 35}}
                     source={{uri: item.urls.full}}
                 >
                     <TouchableOpacity 
@@ -30,7 +32,31 @@ class SinglePost extends React.Component {
                             <AntDesign style={{color: '#222'}} name='arrowleft' size={30}/>
                     </TouchableOpacity>
                 </ImageBackground>
-            </View>
+                <View style={styles.infoContainer}>
+                    <Image 
+                        source={{uri: item.user.profile_image.medium}} 
+                        style={styles.infoHeaderAvatar}
+                        resizeMode={'cover'}
+                    />
+                    <View style={styles.infoHeader}>
+                        <Text style={styles.infoHeaderUsername}>
+                            {item.user.name}
+                        </Text>
+                        <Text style={styles.infoHeaderDate}>
+                            {item.user.username}
+                        </Text>
+                    </View>
+                </View>
+                <TouchableHighlight onPress={() => {
+                    FileSystem.downloadAsync(item.links.download, `${FileSystem.documentDirectory} ${item.id}`).then(({uri}) => {
+                        console.log(uri)
+                    })
+                }} style={styles.downloadButton}>
+                    <Text style={styles.downloadButtonText}>
+                        Download
+                    </Text>
+                </TouchableHighlight>
+            </ScrollView>
         )
     }
 }
--- src/modules/Home/components/SinglePost/style.js
+++ src/modules/Home/components/SinglePost/style.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Dimensions } from "react-native";
-import { iOSUIKit } from "react-native-typography";
+import { human, iOSUIKit, material } from "react-native-typography";
 
 export const styles = StyleSheet.create({
     singlePostWrapper: {
@@ -8,10 +8,46 @@ export const styles = StyleSheet.create({
     },
     singlePostImage: {
         width: Dimensions.get('window').width,
-        height: 400,
+        height: 450,
     },
     backIcon: {
         paddingStart: '5%',
-        paddingTop: '4%'
+        paddingTop: '4%',
+    },
+    infoContainer: {
+        flexDirection: "row",
+        marginHorizontal: 20,
+        marginVertical: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'flex-start',
+    },
+    infoHeader: {
+        flexDirection: "column",
+        marginStart: 10
+    },
+    infoHeaderAvatar: {
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+    },
+    infoHeaderUsername: {
+        ...human.title3Object
+    },
+    infoHeaderDate: {
+        fontWeight: '200',
+        fontSize: 15,
+        color: '#999'
+    },
+    downloadButton: {
+        alignSelf: 'center',
+        alignItems: 'center', 
+        backgroundColor: '#222',
+        width: '90%',
+        paddingVertical: 20,
+        borderRadius: 8,
+    },
+    downloadButtonText: {
+        ...material.buttonWhiteObject
     }
 })
